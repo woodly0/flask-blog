c3e6dbf7799101d776cc4f24b13a56e6414646be
woodly0 <57896921+woodly0@users.noreply.github.com>
2025-01-24 15:14:48 +0100

    complete environment variables

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -4,8 +4,8 @@ import os
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
-    MAIL_SERVER = "smtp.salt.ch"
-    MAIL_PORT = 25
-    MAIL_USE_TLS = False
+    MAIL_SERVER = os.environ.get("MAIL_SERVER")
+    MAIL_PORT = os.environ.get("MAIL_PORT")
+    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS")
     MAIL_USERNAME = os.environ.get("EMAIL_USER")
     MAIL_PASSWORD = os.environ.get("EMAIL_PASS")
